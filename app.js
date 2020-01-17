3c9f394ad2165b3279d46d8d35fe429fc0ff9cd6
Ana-07 <57661647+Ana-07@users.noreply.github.com>
2020-01-17 14:54:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'Welcome to Ibexlabs'
+    title: 'Ibexlabs'
     })
     res.status(200).end();
 })
